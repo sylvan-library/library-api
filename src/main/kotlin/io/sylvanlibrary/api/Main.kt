2f4f90b31e664e03d6aa9782556ac28d4b80784d
Joshua Tompkins <josh@joshtompkins.com>
2018-03-04 18:19:30 -0800

    Add Kotlin, Javalin, and a basic REST skeleton

---
+++ src/main/kotlin/io/sylvanlibrary/api/Main.kt
@@ -0,0 +1,51 @@
+package io.sylvanlibrary.api
+
+import io.javalin.ApiBuilder.get
+import io.javalin.ApiBuilder.path
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+  val app = Javalin.create().apply {
+    port(7000)
+  }.start()
+
+  app.routes {
+    path("status") {
+      get("check") {
+        it.status(200)
+      }
+    }
+
+    path("blocks") {
+      get("/") { ctx ->
+        ctx.result("all blocks!")
+      }
+
+      get(":id") { ctx ->
+        ctx.result("block for id ${ctx.param("id")}")
+      }
+
+      get(":id/sets") { ctx -> ctx.result("sets for block ${ctx.param("id")}") }
+    }
+
+    path("sets") {
+      get("/") { ctx -> ctx.result("all sets!") }
+      get(":abbrev") { ctx -> ctx.result("set ${ctx.param("abbrev")}") }
+      get(":abbrev/cards") { ctx -> ctx.result("cards for set ${ctx.param("abbrev")}") }
+    }
+
+    path("cards") {
+      get("/") {ctx ->
+        val filter = ctx.queryParamOrDefault("filter", "")
+
+        ctx.result("all cards with filter $filter")
+      }
+    }
+
+    path("artists") {
+      get("/") { ctx -> ctx.result("all artists!") }
+      get(":id") { ctx -> ctx.result("artist ${ctx.param("id")}") }
+      get(":id/cards") { ctx -> ctx.result("cards for artist ${ctx.param("id")}") }
+    }
+  }
+}
