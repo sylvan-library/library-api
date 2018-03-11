83885f9e85ffbe9d048ea12e85686a68043daeda
Joshua Tompkins <josh@joshtompkins.com>
2018-03-11 15:26:50 -0700

    Set up DI to use new objects

--- src/main/kotlin/io/sylvanlibrary/api/MainModule.kt
+++ src/main/kotlin/io/sylvanlibrary/api/MainModule.kt
@@ -6,20 +6,17 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.github.salomonbrys.kodein.*
-import io.sylvanlibrary.api.format.FormatDao
-import io.sylvanlibrary.api.format.FormatDaoImpl
-import io.sylvanlibrary.api.format.FormatService
-import io.sylvanlibrary.api.format.FormatServiceImpl
-import io.sylvanlibrary.api.set.SetDao
-import io.sylvanlibrary.api.set.SetDaoImpl
+import io.sylvanlibrary.api.format.FormatMapper
+import io.sylvanlibrary.api.format.FormatMapperImpl
+import io.sylvanlibrary.api.set.SetMapper
+import io.sylvanlibrary.api.set.SetMapperImpl
 import io.sylvanlibrary.api.util.Config
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.postgres.PostgresPlugin
 
 val mainModule = Kodein.Module {
-  bind<FormatService>() with singleton { FormatServiceImpl(instance(), instance(), instance()) }
-  bind<FormatDao>() with singleton { FormatDaoImpl(instance()) }
-  bind<SetDao>() with singleton { SetDaoImpl(instance(), instance()) }
+  bind<FormatMapper>() with singleton { FormatMapperImpl(instance(), instance(), instance()) }
+  bind<SetMapper>() with singleton { SetMapperImpl(instance(), instance()) }
   bind() from singleton { Config() }
 
   bind<Jdbi>() with singleton {
