c886ced4de5952bf0cda1598c362cb355831ef1d
Joshua Tompkins <josh@joshtompkins.com>
2018-03-10 22:49:08 -0700

    Use DI and new objects to build routes

--- src/main/kotlin/io/sylvanlibrary/api/Main.kt
+++ src/main/kotlin/io/sylvanlibrary/api/Main.kt
@@ -1,81 +1,34 @@
 package io.sylvanlibrary.api
 
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
 import io.javalin.ApiBuilder.get
 import io.javalin.ApiBuilder.path
 import io.javalin.Javalin
 import io.javalin.translator.json.JavalinJacksonPlugin
-import io.sylvanlibrary.api.controllers.BlocksController
-import io.sylvanlibrary.api.daos.SetDao
-import org.jdbi.v3.core.Jdbi
-import org.jdbi.v3.postgres.PostgresPlugin
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-
+import io.sylvanlibrary.api.format.FormatService
+import io.sylvanlibrary.api.util.Paths
 
 fun main(args: Array<String>) {
-  val mapper = ObjectMapper()
-  mapper.registerModule(Jdk8Module())
-  mapper.registerModule(JavaTimeModule())
-  mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+  val kodein = Kodein { import(mainModule) }
 
-  JavalinJacksonPlugin.configure(mapper)
+  JavalinJacksonPlugin.configure(kodein.instance())
 
-  val app = Javalin.create().apply {
-    port(7000)
-  }.start()
-
-  val jdbi = Jdbi.create("jdbc:postgresql://localhost:5432/sylvanlibrary?user=sylvanlibrary&password=jacesucks")
-      .installPlugin(PostgresPlugin())
-
-  val setsDao = SetDao(jdbi)
+  val app = Javalin
+      .create()
+      .port(7000)
+      .start()
 
   app.routes {
-    path("status") {
-      get("check") {
-        it.status(200)
-      }
-    }
+    get("status/check") { it.status(200) }
 
-    path("formats") {
-      get("/:name/sets") { ctx ->
-        val format = ctx.param("name")!!
+    path(Paths.Formats.ROOT) {
+      val controller: FormatService = kodein.instance()
 
-        ctx.json(setsDao.allForFormat(format))
-      }
-    }
-
-    path("blocks") {
-      get("/") { ctx ->
-        ctx.json(BlocksController().all())
-      }
-
-      get(":id") { ctx ->
-        ctx.result("block for id ${ctx.param("id")}")
-      }
-
-      get(":id/sets") { ctx -> ctx.result("sets for block ${ctx.param("id")}") }
-    }
-
-    path("sets") {
-      get("/") { ctx -> ctx.result("all sets!") }
-      get(":abbrev") { ctx -> ctx.result("set ${ctx.param("abbrev")}") }
-      get(":abbrev/cards") { ctx -> ctx.result("cards for set ${ctx.param("abbrev")}") }
-    }
-
-    path("cards") {
-      get("/") {ctx ->
-        val filter = ctx.queryParamOrDefault("filter", "")
-
-        ctx.result("all cards with filter $filter")
-      }
-    }
-
-    path("artists") {
-      get("/") { ctx -> ctx.result("all artists!") }
-      get(":id") { ctx -> ctx.result("artist ${ctx.param("id")}") }
-      get(":id/cards") { ctx -> ctx.result("cards for artist ${ctx.param("id")}") }
+      get(Paths.INDEX) { ctx -> ctx.json(controller.all()) }
+      get(Paths.Formats.SHOW) { ctx -> ctx.json(controller.get(ctx.param("name")!!)) }
+      get(Paths.Formats.SETS) { ctx -> ctx.json(controller.sets(ctx.param("name")!!)) }
+      get(Paths.Formats.CARDS) { ctx -> ctx.json(controller.cards(ctx.param("name")!!)) }
     }
   }
 }
