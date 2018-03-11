55684185fc0c401a18f58b6e9bde4e9ea614c589
Joshua Tompkins <josh@joshtompkins.com>
2018-03-11 15:26:58 -0700

    Use the mappers directly for now

--- src/main/kotlin/io/sylvanlibrary/api/Main.kt
+++ src/main/kotlin/io/sylvanlibrary/api/Main.kt
@@ -6,7 +6,8 @@ import io.javalin.ApiBuilder.get
 import io.javalin.ApiBuilder.path
 import io.javalin.Javalin
 import io.javalin.translator.json.JavalinJacksonPlugin
-import io.sylvanlibrary.api.format.FormatService
+import io.sylvanlibrary.api.format.FormatMapper
+import io.sylvanlibrary.api.set.SetMapper
 import io.sylvanlibrary.api.util.Paths
 
 fun main(args: Array<String>) {
@@ -23,12 +24,19 @@ fun main(args: Array<String>) {
     get("status/check") { it.status(200) }
 
     path(Paths.Formats.ROOT) {
-      val controller: FormatService = kodein.instance()
+      val mapper: FormatMapper = kodein.instance()
 
-      get(Paths.INDEX) { ctx -> ctx.json(controller.all()) }
-      get(Paths.Formats.SHOW) { ctx -> ctx.json(controller.get(ctx.param("name")!!)) }
-      get(Paths.Formats.SETS) { ctx -> ctx.json(controller.sets(ctx.param("name")!!)) }
-      get(Paths.Formats.CARDS) { ctx -> ctx.json(controller.cards(ctx.param("name")!!)) }
+      get(Paths.INDEX) { ctx -> ctx.json(mapper.all()) }
+      get(Paths.Formats.SHOW) { ctx -> ctx.json(mapper.get(ctx.param("name")!!)) }
+      get(Paths.Formats.SETS) { ctx -> ctx.status(501) }
+      get(Paths.Formats.CARDS) { ctx -> ctx.status(501) }
+    }
+
+    path(Paths.Sets.ROOT) {
+      val mapper: SetMapper = kodein.instance()
+
+      get(Paths.INDEX) { ctx -> ctx.json(mapper.all()) }
+      get(Paths.Sets.SHOW) { ctx -> ctx.json(mapper.get(ctx.param("abbrev")!!)) }
     }
   }
 }
